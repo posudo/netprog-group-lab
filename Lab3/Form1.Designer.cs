428e4c0bed6e57274a30ec785e1f2893ffecfc0e
Thanh Tung <nguyenthanhtung020405@gmail.com>
2024-10-24 14:33:32 +0700

    Create: Make up form and create Bai4 which is inherited from Lab2-Bai5

--- Lab3/Form1.Designer.cs
+++ Lab3/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.client = new System.Windows.Forms.Button();
+            this.server = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // client
+            // 
+            this.client.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.client.Location = new System.Drawing.Point(114, 172);
+            this.client.Name = "client";
+            this.client.Size = new System.Drawing.Size(144, 35);
+            this.client.TabIndex = 0;
+            this.client.Text = "Client";
+            this.client.UseVisualStyleBackColor = true;
+            // 
+            // server
+            // 
+            this.server.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.server.Location = new System.Drawing.Point(519, 172);
+            this.server.Name = "server";
+            this.server.Size = new System.Drawing.Size(144, 35);
+            this.server.TabIndex = 1;
+            this.server.Text = "Server";
+            this.server.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.server);
+            this.Controls.Add(this.client);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button client;
+        private System.Windows.Forms.Button server;
     }
 }
 
