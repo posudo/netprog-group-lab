b654a5cccd0e32eb79558f6b71b09bd71bfb90c1
NQV-DEV <quocvietnguyen104@gmail.com>
2024-10-29 18:37:54 +0700

    tao form dieu huong

---
+++ Lab3/Formdieuhuongcs.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Lab3
+{
+    partial class Formdieuhuongcs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBai3 = new System.Windows.Forms.Button();
+            this.btnBai4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBai3
+            // 
+            this.btnBai3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai3.Location = new System.Drawing.Point(350, 229);
+            this.btnBai3.Name = "btnBai3";
+            this.btnBai3.Size = new System.Drawing.Size(233, 45);
+            this.btnBai3.TabIndex = 0;
+            this.btnBai3.Text = "Bài 3";
+            this.btnBai3.UseVisualStyleBackColor = true;
+            this.btnBai3.Click += new System.EventHandler(this.btnBai3_Click);
+            // 
+            // btnBai4
+            // 
+            this.btnBai4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai4.Location = new System.Drawing.Point(350, 308);
+            this.btnBai4.Name = "btnBai4";
+            this.btnBai4.Size = new System.Drawing.Size(233, 45);
+            this.btnBai4.TabIndex = 1;
+            this.btnBai4.Text = "Bài 4";
+            this.btnBai4.UseVisualStyleBackColor = true;
+            this.btnBai4.Click += new System.EventHandler(this.btnBai4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(881, 76);
+            this.panel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(400, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 41);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "LAB 3";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(350, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 45);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Bài 5";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lab3.Properties.Resources.logodaidien1;
+            this.pictureBox1.Location = new System.Drawing.Point(87, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(724, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Formdieuhuongcs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(881, 469);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnBai4);
+            this.Controls.Add(this.btnBai3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Formdieuhuongcs";
+            this.Text = "Formdieuhuongcs";
+            this.Load += new System.EventHandler(this.Formdieuhuongcs_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBai3;
+        private System.Windows.Forms.Button btnBai4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Lab3/Formdieuhuongcs.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Formdieuhuongcs : Form
+    {
+        public Formdieuhuongcs()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBai3_Click(object sender, EventArgs e)
+        {
+            Bai3 bai3 = new Bai3();
+            bai3.Show();
+        }
+
+        private void btnBai4_Click(object sender, EventArgs e)
+        {
+            Bai4 bai4 = new Bai4();
+            bai4.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Lab03_Bai05_Server bai5 = new Lab03_Bai05_Server();
+            bai5.Show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Formdieuhuongcs_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
