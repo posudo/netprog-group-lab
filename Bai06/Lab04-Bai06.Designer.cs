3276ba5efb155d7749ae71832f5a7157076defa0
posudo <mmttriet@gmail.com>
2024-11-05 20:37:33 +0700

    chore: add Bai06 controls, change form's name

---
+++ Bai06/Lab04-Bai06.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Bai06
+{
+    partial class Bai06
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbURL = new System.Windows.Forms.TextBox();
+            this.tbToken = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SHOW";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tbURL
+            // 
+            this.tbURL.Location = new System.Drawing.Point(98, 12);
+            this.tbURL.Name = "tbURL";
+            this.tbURL.Size = new System.Drawing.Size(216, 20);
+            this.tbURL.TabIndex = 1;
+            this.tbURL.Text = "https://nt106.uitiot.vn/api/v1/user/me";
+            // 
+            // tbToken
+            // 
+            this.tbToken.Location = new System.Drawing.Point(98, 38);
+            this.tbToken.Name = "tbToken";
+            this.tbToken.Size = new System.Drawing.Size(216, 20);
+            this.tbToken.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(388, 201);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Token";
+            // 
+            // Bai06
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 276);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tbToken);
+            this.Controls.Add(this.tbURL);
+            this.Controls.Add(this.button1);
+            this.Name = "Bai06";
+            this.Text = "Bai 6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbURL;
+        private System.Windows.Forms.TextBox tbToken;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Bai06/Lab04-Bai06.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace Bai06
+{
+    public partial class Bai06 : Form
+    {
+        public Bai06()
+        {
+            InitializeComponent();
+        }
+        private static readonly HttpClient httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(@"https://nt106.uitiot.vn")
+        };
+
+
+
+    }
+}
