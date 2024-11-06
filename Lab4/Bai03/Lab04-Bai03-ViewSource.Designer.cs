4ec7e2ee664654d45274bcd4a3d397990b62efbd
Posu <mmttriet@gmail.com>
2024-11-07 00:33:22 +0700

    feat: finish Bai03

---
+++ Lab4/Bai03/Lab04-Bai03-ViewSource.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace web_server
+{
+    partial class Lab04_Bai03_ViewSource
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbShow = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbShow
+            // 
+            this.rtbShow.Location = new System.Drawing.Point(12, 12);
+            this.rtbShow.Name = "rtbShow";
+            this.rtbShow.Size = new System.Drawing.Size(776, 426);
+            this.rtbShow.TabIndex = 0;
+            this.rtbShow.Text = "";
+            // 
+            // Lab04_Bai03_ViewSource
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbShow);
+            this.Name = "Lab04_Bai03_ViewSource";
+            this.Text = "Lab04_Bai03_ViewSource";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbShow;
+    }
+}
