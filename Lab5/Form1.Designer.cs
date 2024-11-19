0ca2ebc82483f566c0542a77c0c36a3b0167520c
Thanh Tung <nguyenthanhtung020405@gmail.com>
2024-11-19 20:14:17 +0700

    Merge: rebase main to submain01

---
+++ Lab5/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.send = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.from_email = new System.Windows.Forms.TextBox();
+            this.to_email = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.subject = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.body = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // send
+            // 
+            this.send.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.send.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.send.Location = new System.Drawing.Point(35, 29);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(79, 39);
+            this.send.TabIndex = 0;
+            this.send.Text = "Send";
+            this.send.UseVisualStyleBackColor = false;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(137, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To:";
+            // 
+            // from_email
+            // 
+            this.from_email.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.from_email.Location = new System.Drawing.Point(212, 19);
+            this.from_email.Name = "from_email";
+            this.from_email.Size = new System.Drawing.Size(280, 33);
+            this.from_email.TabIndex = 3;
+            // 
+            // to_email
+            // 
+            this.to_email.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.to_email.Location = new System.Drawing.Point(212, 61);
+            this.to_email.Name = "to_email";
+            this.to_email.Size = new System.Drawing.Size(280, 33);
+            this.to_email.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Subject:";
+            // 
+            // subject
+            // 
+            this.subject.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subject.Location = new System.Drawing.Point(142, 125);
+            this.subject.Name = "subject";
+            this.subject.Size = new System.Drawing.Size(280, 33);
+            this.subject.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 26);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Body:";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // body
+            // 
+            this.body.Location = new System.Drawing.Point(142, 177);
+            this.body.Name = "body";
+            this.body.Size = new System.Drawing.Size(646, 261);
+            this.body.TabIndex = 10;
+            this.body.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.body);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.subject);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.to_email);
+            this.Controls.Add(this.from_email);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.send);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox from_email;
+        private System.Windows.Forms.TextBox to_email;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox subject;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox body;
+    }
+}
+
